63daaf1fbdea081309aff019a912314067d4a92b
WVAviator <wvaviator@gmail.com>
2022-02-09 13:04:45 -0600

    Refactored menu icon / fixed hero pointer events

--- components/header/Hamburger.js
+++ components/header/Hamburger.js
@@ -2,12 +2,14 @@ import Link from "next/link";
 import styles from "./Hamburger.module.scss";
 
 const Hamburger = ({ open, setOpen }) => {
-	const activeClass = open ? styles.open : styles.closed;
+	const activeClass = `${styles.menuIcon} ${
+		open ? styles.open : styles.closed
+	}`;
 
 	return (
-		<a className={styles.menuIcon} onClick={() => setOpen(!open)}>
+		<div className={styles.menuIconContainer} onClick={() => setOpen(!open)}>
 			<div className={activeClass}></div>
-		</a>
+		</div>
 	);
 };
 
