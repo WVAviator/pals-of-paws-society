10dae7836cf5c5f505355b113432f1427d15f8ee
WVAviator <wvaviator@gmail.com>
2022-02-13 13:15:03 -0600

    added server code to concat shelterluv and pf apis

--- pages/api/animals/index.ts
+++ pages/api/animals/index.ts
@@ -1,9 +1,26 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Petfinder } from "../../../src/api/Petfinder";
+import { convertPetfinderAnimal } from "../../../src/api/PetfinderAdapter";
+import getShelterluvAnimals from "../../../src/api/Shelterluv";
+import { convertShelterluvAnimal } from "../../../src/api/ShelterluvAdapter";
 
 const pf = Petfinder.getInstance();
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-	const animals = await pf.getAnimals();
+	const animals = await getAnimals();
 	res.send("Attempted to retrieve animals.\n" + JSON.stringify(animals));
+};
+
+const getAnimals = async () => {
+	const pfAnimals = await pf.getAnimals();
+	const shelterluvAnimals = await getShelterluvAnimals();
+
+	const convertedPetfinderAnimals = pfAnimals.map((animal) =>
+		convertPetfinderAnimal(animal)
+	);
+	const convertedShelterluvAnimals = shelterluvAnimals.map((animal) =>
+		convertShelterluvAnimal(animal)
+	);
+
+	return [...convertedPetfinderAnimals, ...convertedShelterluvAnimals];
 };
