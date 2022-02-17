3af28f4073305533234286a67950866c0d7d7a64
WVAviator <wvaviator@gmail.com>
2022-02-17 15:18:41 -0600

    Font optimization

--- components/layout/DefaultHead.js
+++ components/layout/DefaultHead.js
@@ -16,14 +16,10 @@ const DefaultHead = () => {
 			<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			<link rel="icon" href="/POP_Favicon.png" />
-			<link
-				href="https://fonts.googleapis.com/css2?family=Signika+Negative:wght@300;400;500;600;700&display=swap"
-				rel="stylesheet"
-			/>
 			<link rel="preconnect" href="https://fonts.googleapis.com" />
 			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 			<link
-				href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
+				href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Signika+Negative:wght@400;600&display=swap"
 				rel="stylesheet"
 			/>
 		</Head>
