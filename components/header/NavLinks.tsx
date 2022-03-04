4f2e164866809927491a1733143975d5afb0e18a
WVAviator <wvaviator@gmail.com>
2022-03-04 12:32:57 -0600

    Fix navlinks for SEO

--- components/header/NavLinks.tsx
+++ components/header/NavLinks.tsx
@@ -4,29 +4,29 @@ interface NavLinksProps {
 	onClick?: () => void;
 }
 
-const NavLinks = ({ onClick = () => {} }: NavLinksProps) => {
+const NavLinks = ({ onClick = () => { } }: NavLinksProps) => {
 	return (
 		<ul>
-			<Link href="/" passHref>
-				<li onClick={onClick}>
-					<a>Home</a>
-				</li>
-			</Link>
-			<Link href="/about" passHref>
-				<li onClick={onClick}>
-					<a>About</a>
-				</li>
-			</Link>
-			<Link href="/fundraisers" passHref>
-				<li onClick={onClick}>
-					<a>Fundraisers</a>
-				</li>
-			</Link>
-			<Link href="/adopt" passHref>
-				<li onClick={onClick}>
-					<a>Adopt</a>
-				</li>
-			</Link>
+
+			<li onClick={onClick}>
+				<Link href="/" passHref><a>Home</a></Link>
+			</li>
+
+
+			<li onClick={onClick}>
+				<Link href="/about" passHref><a>About</a></Link>
+			</li>
+
+
+			<li onClick={onClick}>
+				<Link href="/fundraisers" passHref><a>Fundraisers</a></Link>
+			</li>
+
+
+			<li onClick={onClick}>
+				<Link href="/adopt" passHref><a>Adopt</a></Link>
+			</li>
+
 		</ul>
 	);
 };
