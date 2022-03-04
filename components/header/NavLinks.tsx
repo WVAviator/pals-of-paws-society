80e8ead72353703ef9f9a4dc406c1b59cf8e4c38
WVAviator <wvaviator@gmail.com>
2022-03-04 12:45:49 -0600

    More adjustments to navlinks

--- components/header/NavLinks.tsx
+++ components/header/NavLinks.tsx
@@ -9,22 +9,22 @@ const NavLinks = ({ onClick = () => { } }: NavLinksProps) => {
 		<ul>
 
 			<li onClick={onClick}>
-				<Link href="/" passHref><a>Home</a></Link>
+				<Link href="/" ><a>Home</a></Link>
 			</li>
 
 
 			<li onClick={onClick}>
-				<Link href="/about" passHref><a>About</a></Link>
+				<Link href="/about" ><a>About</a></Link>
 			</li>
 
 
 			<li onClick={onClick}>
-				<Link href="/fundraisers" passHref><a>Fundraisers</a></Link>
+				<Link href="/fundraisers" ><a>Fundraisers</a></Link>
 			</li>
 
 
 			<li onClick={onClick}>
-				<Link href="/adopt" passHref><a>Adopt</a></Link>
+				<Link href="/adopt" ><a>Adopt</a></Link>
 			</li>
 
 		</ul>
