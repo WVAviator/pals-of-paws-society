0997671e413a9ad3b4f51526bde5a347af4e1503
WVAviator <wvaviator@gmail.com>
2022-03-07 12:56:07 -0600

    update MDXImage styles

--- components/mdx/MDXImage.js
+++ components/mdx/MDXImage.js
@@ -2,20 +2,29 @@ import Image from "next/image";
 import styles from "./MDXImage.module.scss";
 
 const MDXImage = (props) => {
-	let floatStyle = { float: "none" };
-	if (props.float) {
-		floatStyle = {
-			float: props.float,
-			width: props.floatWidth ?? "33%",
-			margin: `1rem ${props.float === "right" ? "0" : "3rem"} 1rem ${
-				props.float === "right" ? "3rem" : "0"
-			}`, //Flush with the content margins on one side
-		};
-	}
+	// let floatStyle = { float: "none" };
+	// if (props.float) {
+	// 	floatStyle = {
+	// 		float: props.float,
+	// 		width: props.floatWidth ?? "33%",
+	// 		margin: `1rem ${props.float === "right" ? "0" : "3rem"} 1rem ${
+	// 			props.float === "right" ? "3rem" : "0"
+	// 		}`, //Flush with the content margins on one side
+	// 	};
+	// }
 
 	return (
-		<div className={styles.image} style={floatStyle}>
+		<div className={styles.image}>
 			<Image {...props} />
+			<style jsx>{`
+				@media (min-width: 40rem) {
+					div {
+						float: ${props.float ?? "none"};
+						width: ${props.floatWidth ?? "100%"};
+						margin: 1rem ${props.float === "right" ? "0" : "3rem"} 1rem ${props.float === "left" ? "0" : "3rem"};
+					}
+				}
+			`}</style>
 		</div>
 	);
 };
