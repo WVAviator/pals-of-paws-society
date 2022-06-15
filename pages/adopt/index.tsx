2d24d7b9b8559418a8faebc7fb8eca830216b2df
WVAviator <wvaviator@gmail.com>
2022-06-14 21:24:27 -0500

    refactored adopt page to only serve page 1 statically and fetch the rest on the client side

--- pages/adopt/index.tsx
+++ pages/adopt/index.tsx
@@ -1,6 +1,7 @@
-import { GetServerSideProps } from "next";
+import axios from "axios";
+import { GetStaticProps } from "next";
 import { useRouter } from "next/router";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import PetCardContent from "../../components/page-sections/PetCardContent";
 import PetDisplay from "../../components/page-sections/PetDisplay";
 import redis from "../../src/redis";
@@ -13,40 +14,54 @@ interface AdoptProps {
 const Adopt = ({ animals, updatedAt }: AdoptProps) => {
 	console.log("Animals last updated at: ", updatedAt);
 
-	const router = useRouter();
-	const selectedAnimal = useRef<Animal>(
-		animals.find((a) => a.id === router.query.animal) || null
-	);
-	const page = Number(router.query.page) || 1;
+	const [currentAnimals, setCurrentAnimals] = useState(animals);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [currentAnimal, setCurrentAnimal] = useState<Animal | null>(null);
+	const [savedScrollPosition, setSavedScrollPosition] = useState(0);
 
-	const routeToAnimal = (animal: Animal) => {
-		router.push(`/adopt?animal=${animal.id}`, null, { shallow: true });
+	useEffect(() => {
+		const getAnimalsForPage = async () => {
+			const newAnimals = (await axios.get(`/api/animals`))
+				.data as Animal[];
+			console.log(newAnimals);
+			setCurrentAnimals(newAnimals);
+		};
+
+		getAnimalsForPage();
+	}, []);
+
+	const goToAnimal = (animal: Animal) => {
+		setSavedScrollPosition(window.scrollY);
+		setCurrentAnimal(animal);
+		window.scrollTo(0, 0);
 	};
 
-	const updatePage = (newPage: number) => {
-		router.push(`/adopt?page=${newPage}`, null, { shallow: true });
+	const returnToSearch = () => {
+		setCurrentAnimal(null);
+		setTimeout(() => window.scrollTo(0, savedScrollPosition), 0);
 	};
 
-	const routeBack = () => {
-		router.back();
+	const changePage = (page: number) => {
+		setCurrentPage(page);
+		window.scrollTo(0, 0);
 	};
 
 	return (
 		<div>
-			{selectedAnimal.current ? (
+			{currentAnimal ? (
 				<>
 					<PetDisplay
-						animal={selectedAnimal.current}
-						routeBack={routeBack}
+						animal={currentAnimal}
+						routeBack={returnToSearch}
 					/>
 				</>
 			) : (
 				<>
 					<PetCardContent
-						animals={animals}
-						page={page}
-						setPage={updatePage}
-						routeToAnimal={routeToAnimal}
+						animals={currentAnimals}
+						page={currentPage}
+						setPage={changePage}
+						routeToAnimal={goToAnimal}
 					/>
 				</>
 			)}
@@ -54,17 +69,18 @@ const Adopt = ({ animals, updatedAt }: AdoptProps) => {
 	);
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 	console.log("Retrieving static props...", new Date());
 
-	const jsonAnimals = await redis.get("animals");
+	const jsonAnimals = await redis.get("animals:1");
 	const animals: Animal[] = (await JSON.parse(jsonAnimals)) || [];
 
 	return {
 		props: {
 			animals,
 			updatedAt: new Date().toISOString(),
-		}
+		},
+		revalidate: 3600,
 	};
 };
 
