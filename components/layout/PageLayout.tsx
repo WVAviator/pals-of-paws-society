edd963abbd0f0ae98900dd438ae439309f63f398
Wvaviator <wvaviator@gmail.com>
2023-06-17 13:28:01 -0500

    add default seo to all pages, replacing DefaultHead component

--- components/layout/PageLayout.tsx
+++ components/layout/PageLayout.tsx
@@ -1,6 +1,7 @@
-import Header from "../header/Header";
+import { DefaultSeo } from "next-seo";
 import Footer from "../footer/Footer";
-import DefaultHead from "./DefaultHead";
+import Header from "../header/Header";
+import ogImage from "/images/og/pop-og.png";
 
 interface PageLayoutProps {
 	children: React.ReactNode;
@@ -9,7 +10,27 @@ interface PageLayoutProps {
 const PageLayout = ({ children }: PageLayoutProps) => {
 	return (
 		<>
-			<DefaultHead />
+			<DefaultSeo
+				title="Pals of Paws Society"
+				description="Pals of Paws Society is a nonprofit organization dedicated to fighting the pet overpopulation problem in Northwest Mississippi. We provide adoption, animal transport, and veterinary financial assistance for shelters, rescue groups, and individuals throughout Desoto, Tate, and Tunica counties, as well as Memphis, Tennessee, from our headquarters in Hernando."
+				openGraph={{
+					type: "website",
+					locale: "en_US",
+					url: "https://www.palsofpawssociety.org/",
+					site_name: "Pals of Paws Society",
+					title: "Pals of Paws Society",
+					description:
+						"Pals of Paws Society is a nonprofit organization dedicated to fighting the pet overpopulation problem in Northwest Mississippi. We provide adoption, animal transport, and veterinary financial assistance for shelters, rescue groups, and individuals throughout Desoto, Tate, and Tunica counties, as well as Memphis, Tennessee, from our headquarters in Hernando.",
+					images: [
+						{
+							url: "https://www.palsofpawssociety.org/images/og/pop-og.png",
+							width: 1200,
+							height: 630,
+							alt: "Pals of Paws Society",
+						},
+					],
+				}}
+			/>
 			<Header />
 			<main id="main">{children}</main>
 			<Footer />
