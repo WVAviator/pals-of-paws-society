06b107a99d349ba241e2e3f258744eba54be1699
WVAviator <wvaviator@gmail.com>
2025-04-06 15:53:44 -0500

    add petco lost page

---
+++ pages/lost/index.tsx
@@ -0,0 +1,11 @@
+import PetcoLost from "../../components/ui/PetcoLost";
+
+const PetcoLostPage = () => {
+	return (
+		<div style={{ minHeight: "100svh" }}>
+			<PetcoLost url="https://petcolove.org/lost/shelters/pals-of-paws-society/4019/map/?token=acc79ff12b51e23cb4f6eaa11185696c&latitude=34.823987&longitude=-89.9937018&utm_medium=referral&utm_source=shelter&zoom=11&utm_campaign=lovelost-map-widget-shelter-4019&utm_term=map-view" />
+		</div>
+	);
+};
+
+export default PetcoLostPage;
